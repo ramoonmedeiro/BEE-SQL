64f55963dbc95c670947ab2f2bfeebf0df8b671d
ramoonmedeiro <r.medeiro10@gmail.com>
2022-07-03 16:52:15 -0300

    Adicionando mais exercícios

---
+++ 2737.sql
@@ -0,0 +1,7 @@
+SELECT name, customers_number FROM lawyers
+WHERE customers_number = (SELECT MAX(customers_number) FROM lawyers)
+UNION ALL
+SELECT name, customers_number FROM lawyers
+WHERE customers_number = (SELECT MIN(customers_number) FROM lawyers)
+UNION ALL
+SELECT 'Average' AS name, round(AVG(customers_number), 0) AS customers_number FROM lawyers;
